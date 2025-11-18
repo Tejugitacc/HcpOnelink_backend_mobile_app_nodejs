497c8ea5b137cff5fd8a6e1a4e6c435a38600d0f
tejashwini K R <ahwiniteju@gmail.com>
2025-11-18 12:51:00 +0530

    folder structure added

--- server.js
+++ server.js
@@ -1,70 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
-
 const app = express();
+
 app.use(express.json());
 
 // CORS for development
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
-// ------------------------------
-// Appian LOGIN Proxy
-// ------------------------------
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+const corsOptions = {
+  origin: '*'
+};
+app.use(cors(corsOptions));
 
-  console.log('========', req.body);
+require('./app/routes/auth.routes')(app);
 
-  try {
-    // Prepare Basic Auth (Appian username + Appian password)
-    const base64Creds = Buffer.from(`${username}:${password}`).toString(
-      'base64'
-    );
-
-    const response = await fetch(
-      'https://dsi-hcp-dev.appiancloud.com/suite/webapi/mobileuserlogin',
-      {
-        method: 'POST',
-
-        headers: {
-          Authorization: `Basic ${base64Creds}`, // <<< UPDATED
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-
-        body: JSON.stringify({ username, password })
-      }
-    );
-
-    console.log('======== response', response);
-    console.log('Headers:', response.headers);
-
-    const buffer = await response.arrayBuffer();
-    const text = Buffer.from(buffer).toString('utf8');
-
-    let data;
-    try {
-      data = JSON.parse(text);
-    } catch {
-      data = { raw: text };
-    }
-
-    console.log('DATA :', data);
-    res.status(response.status).json(data);
-  } catch (err) {
-    console.error('Appian Proxy Error:', err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// ------------------------------
-app.listen(process.env.PORT || 4000, () =>
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${process.env.PORT || 4000}`)
 );
